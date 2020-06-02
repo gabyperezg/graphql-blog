04dee6a98ed1d30d2d5cda417799d1d3db5dc013
PichuPerez <gabyperez.dev@gmail.com>
2020-06-01 22:17:02 -0500

    Added extended post subscription

--- src/resolvers/Mutations.js
+++ src/resolvers/Mutations.js
@@ -77,7 +77,12 @@ const Mutation = {
     db.posts.push(post)
 
     if (args.post.published) {
-      pubsub.publish('post', { post })
+      pubsub.publish('post', {
+        post: {
+          mutation: 'CREATED',
+          data: post,
+        },
+      })
     }
 
     return post
